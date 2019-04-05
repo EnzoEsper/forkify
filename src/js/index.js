4f13cd6b8964bd0a52a493e51f811a759619a28d
enzoesper <enzoesper@hotmail.com>
2019-04-05 18:19:50 -0300

    construccion del modelo Search

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,5 @@
-// Global app controller
-import axios from 'axios'
+import Search from './models/Search';
 
-async function getResults(query){
-  const key = '8a363216f63612e94e11adc5dbac740e';
-
-  try {
-    const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
-    const recipes = res.data.recipes;
-    console.log(recipes);
-  } catch (error) {
-    console.log(error);
-  }
-  
-};
-
-getResults('tomato pasta');
-
-
-
-// https://www.food2fork.com/api/search 
-// 8a363216f63612e94e11adc5dbac740e 
+const search = new Search('pizza');
+console.log(search);
+search.getResults();
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,1 +1,23 @@
-export default 'I am an exported string :)'
+import axios from 'axios'
+
+export default class Search {
+  constructor(query){
+    this.query = query;
+  }
+
+  async getResults(){
+    const key = '8a363216f63612e94e11adc5dbac740e';
+  
+    try {
+      const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+      this.result = res.data.recipes;
+      /* console.log(this.result); */
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
+
+
+// https://www.food2fork.com/api/search 
+// 8a363216f63612e94e11adc5dbac740e 
