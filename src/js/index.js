d6ba17f8ced752ee134fb081fbff8a1fe14a0f89
enzoesper <enzoesper@hotmail.com>
2019-04-05 19:00:44 -0300

    construccion del Controlador search

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,31 @@
 import Search from './models/Search';
 
-const search = new Search('pizza');
-console.log(search);
-search.getResults();
+/* **ESTADO GLOBAL DE LA APP **
+- Search object
+- Current recipe object
+- Shopping list object
+- Liked recipies */
+const state = {};
+
+const controlSearch = async () => {
+  // 1. get query from the view
+  const query = 'pizza'; //hacer luego
+  
+  if(query){
+    // 2. New search object and add to state
+    state.search = new Search(query);
+
+    // 3. Prepare the UI interface for what is going to happen
+
+    // 4. Search for recipies
+    await state.search.getResults()
+
+    // 5. Render results on the UI
+    console.log(state.search.result);
+  }
+};
+
+document.querySelector('.search').addEventListener('submit', e => {
+  e.preventDefault();
+  controlSearch();
+});
