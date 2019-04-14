9aefcd7dee40d80494705c9c7b3aa7008c4441e6
enzoesper <enzoesper@hotmail.com>
2019-04-14 19:09:34 -0300

    Actualizando porciones por receta

--- src/js/index.js
+++ src/js/index.js
@@ -99,3 +99,20 @@ window.addEventListener('hashchange', controlRecipe);
 window.addEventListener('load', controlRecipe); 
 ESTO SE PUEDE HACER EN UNA SOLA LINEA DE CODIGO*/
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
+
+// Handling recipe button clicks
+elements.recipe.addEventListener('click', e => {
+  if (e.target.matches('.btn-decrease , .btn-decrease *')) {
+    // Decrease button is clicked
+    if (state.recipe.servings > 1) {
+      state.recipe.updateServings('dec');
+      recipeView.updateServingsIngredients(state.recipe);
+    }
+  } else if (e.target.matches('.btn-increase, .btn-increase *')) {
+    // Decrease button is clicked
+    state.recipe.updateServings('inc');
+    recipeView.updateServingsIngredients(state.recipe);
+  };
+  
+  console.log(state.recipe);
+})
