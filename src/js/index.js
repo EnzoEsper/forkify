a62cd3a000754179b80ea2fa4845a1317f73c1aa
enzoesper <enzoesper@hotmail.com>
2019-04-14 19:48:36 -0300

    Implementacion shopping list

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
+import List from './models/List';
 import * as searchView from './views/searchView'
 import * as recipeView from './views/recipeView'
 import {elements, renderLoader, clearLoader, elementStrings} from './views/base';
@@ -77,7 +78,6 @@ const controlRecipe = async () => {
     try {
       // Obtener los datos del recipe
       await state.recipe.getRecipe();
-      console.log(state.recipe.ingredients);
       state.recipe.parseIngredients();
   
       // Calcular porciones(serving) y tiempo(time)
@@ -114,5 +114,6 @@ elements.recipe.addEventListener('click', e => {
     recipeView.updateServingsIngredients(state.recipe);
   };
   
-  console.log(state.recipe);
-})
+});
+
+window.l = new List();
