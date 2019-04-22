1df10494bf97e47e7c03615dfd35d05f62468081
enzoesper <enzoesper@hotmail.com>
2019-04-21 21:07:43 -0300

    correccion de la funcion para dar formato numeros

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -10,15 +10,16 @@ const formatCount = count => {
     if (count) {
         // count = 2.5 --> 2 1/2
         // count = 0.5 --> 1/2
-        const [int, dec] = count.toString().split('.');
+        const newCount = Math.round(count * 10000) / 10000;
+        const [int, dec] = newCount.toString().split('.');
 
-        if (!dec) return count;
+        if (!dec) return newCount;
 
         if (parseInt(int) === 0) {
-            const fr = new Fraction(count);
+            const fr = new Fraction(newCount);
             return `${fr.numerator}/${fr.denominator}`;
         } else {
-            const fr = new Fraction(count - parseInt(int));
+            const fr = new Fraction(newCount - parseInt(int));
             return `${int} ${fr.numerator}/${fr.denominator}`;
         }
     };
